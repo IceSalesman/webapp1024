7dabec7eebea70c8b9bf025970a27f5a1b40b6e3
IceSalesman <datagames112@gmail.com>
2024-03-11 13:19:59 +0200

    yotdsf,df

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,5 @@
 <script>
-  export let name = 'SvelteKit';
+	import Auth from "../components/Auth.svelte";
+
 </script>
-
-<main>
-  <h1>Welcome {name}!</h1>
-  <p>This is the intro of our web app.</p>
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 2rem;
-  }
-
-  h1 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-  }
-</style>
+<Auth/>
