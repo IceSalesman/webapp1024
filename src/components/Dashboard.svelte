dff30ce2979174c3f6090b1ca6991d853716efd1
IceSalesman <datagames112@gmail.com>
2024-04-04 19:55:26 +0300

    Update Dashboard navigation link and add logout functionality

--- src/components/Dashboard.svelte
+++ src/components/Dashboard.svelte
@@ -134,12 +134,13 @@
 
 <nav class="bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 border-gray-600">
 	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-		<a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+		<a href="/privatedashboard" class="flex items-center space-x-3 rtl:space-x-reverse">
 			<img src="https://www.svgrepo.com/show/15181/volleyball.svg" class="h-8" alt="Logo" />
 			<span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Volejols</span>
 		</a>
 		<div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
 			<button
+				on:click={authHandlers.logout}
 				type="button"
 				class="text-white w-20 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
 				>Iziet</button
