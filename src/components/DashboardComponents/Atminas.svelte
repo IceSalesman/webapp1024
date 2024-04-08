1df0ff3e4833239ec6b365d6b60d79d775dea271
IceSalesman <datagames112@gmail.com>
2024-04-08 20:52:24 +0300

    Add media upload functionality and display uploaded media

--- src/components/DashboardComponents/Atminas.svelte
+++ src/components/DashboardComponents/Atminas.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { storage } from '$lib/firebase/firebase.client';
-	import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+	import { ref, uploadBytesResumable, getDownloadURL, listAll } from 'firebase/storage';
+	import { onMount } from 'svelte';
 
 	// @ts-ignore
 	let file;
-    let uploadProgress = 0;
-    let downloadUrl = '';
+	let uploadProgress = 0;
+	let downloadUrl = '';
+
+	let mediaList: any[] = [];
 
 	const MAX_FILE_SIZE = 10485760; // 10MB
-
 
 	// @ts-ignore
 
@@ -17,14 +19,19 @@
 		if (file.size > MAX_FILE_SIZE) {
 			alert('Fails ir pārāk liels, limits ir 10MB');
 			file = null;
+		} else if (!file.type.startsWith('image/') && !file.type.startsWith('video/')) {
+			alert('Tikai bildes un video ir atļautas');
+			file = null;
+		} else {
+			uploadFile();
 		}
 	}
 
-	function uploadFile() {
+	async function uploadFile() {
 		// @ts-ignore
 		if (!file) return;
-        
-        //sitas mesls negrib stradat.
+
+		//sitas mesls negrib stradat.
 		const storageRef = ref(storage, `media/${file.name}`);
 
 		const uploadTask = uploadBytesResumable(storageRef, file);
@@ -32,28 +39,77 @@
 		uploadTask.on(
 			'state_changed',
 			(snapshot) => {
-				
 				const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
 				uploadProgress = progress;
 			},
 
 			(error) => {
-				
 				console.error('Upload failed:', error);
 			},
+
 			() => {
-				
 				getDownloadURL(uploadTask.snapshot.ref).then((url) => {
 					downloadUrl = url;
 					console.log('File available at', url);
 				});
 			}
 		);
+		window.location.reload();
 	}
+
+	let fileInput: HTMLInputElement;
+	function triggerFileSelect() {
+		fileInput.click();
+	}
+
+	onMount(async () => {
+		const storageRef = ref(storage, 'media');
+		const res = await listAll(storageRef);
+		mediaList = await Promise.all(
+			res.items.map(async (item) => {
+				const url = await getDownloadURL(item);
+				return { url };
+			})
+		);
+	});
 </script>
 
-<main class="bg-gray-800 text-gray-100 flex flex-col justify-center items-center h-screen">
-    PAŠLAIK NESTRĀDĀ, VARAT MĒGINĀT, BET NEKAS NENOTIKS. 
-	<input type="file" on:change={handleFileChange} />
-	<button on:click={uploadFile}>Upload</button>
+<main
+	class="bg-gray-800 text-gray-100 flex flex-col justify-start items-stretch min-h-screen p-4 pt-20"
+	style="height: calc(100vh - 5rem);"
+>
+	<div class="items-center">
+		<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+			{#each mediaList as media}
+				<div>
+					<img src={media.url} alt="Uploaded media" class="w-full h-49 object-contain" />
+				</div>
+			{/each}
+			<input type="file" bind:this={fileInput} on:change={handleFileChange} style="display: none" />
+
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<div class="flex items-center justify-center">
+				<svg
+				on:click={triggerFileSelect}
+				xmlns="http://www.w3.org/2000/svg"
+				height="100px"
+				viewBox="0 0 24 24"
+				width="100px"
+				fill="#000000"
+				class="cursor-pointer"
+				><path d="M0 0h24v24H0V0z" fill="none" /><path
+					d="M21.02 5H19V2.98c0-.54-.44-.98-.98-.98h-.03c-.55 0-.99.44-.99.98V5h-2.01c-.54 0-.98.44-.99.98v.03c0 .55.44.99.99.99H17v2.01c0 .54.44.99.99.98h.03c.54 0 .98-.44.98-.98V7h2.02c.54 0 .98-.44.98-.98v-.04c0-.54-.44-.98-.98-.98zM16 9.01V8h-1.01c-.53 0-1.03-.21-1.41-.58-.37-.38-.58-.88-.58-1.44 0-.36.1-.69.27-.98H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-8.28c-.3.17-.64.28-1.02.28-1.09-.01-1.98-.9-1.98-1.99zM15.96 19H6c-.41 0-.65-.47-.4-.8l1.98-2.63c.21-.28.62-.26.82.02L10 18l2.61-3.48c.2-.26.59-.27.79-.01l2.95 3.68c.26.33.03.81-.39.81z"
+				/></svg
+			>
+			</div>
+			
+
+			{#if uploadProgress > 0 && uploadProgress < 100}
+				<progress max="100" value={uploadProgress}></progress>
+			{:else if uploadProgress === 100}
+				<p>Gatavs</p>
+			{/if}
+		</div>
+	</div>
 </main>
--- src/components/DashboardComponents/Kontakti.svelte
+++ src/components/DashboardComponents/Kontakti.svelte
@@ -3,6 +3,8 @@
 <main class="bg-gray-800 text-gray-100 flex justify-center items-center h-screen">
     <div class="flex flex-col items-center">
         <h1 class="text-4xl font-bold">Kontakti</h1>
-        <p class="mt-4">Man slinkums taisit :)</p>
+        <p class="mt-4">Sekretāre: Kristīna Ņesterova (Tel. Nr. - +371 27727977)</p>
+        <p class="mt-4">IT komanda: hz ietagojat Lielos Koda Licējus Svētajos Bambāļos</p>
+        
     </div>
 </main>
